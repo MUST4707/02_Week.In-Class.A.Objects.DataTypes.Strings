c57f68ed71741e8aa5325e7e1b72910b6e5b1952
Sam Wells <32932600+sllewmas@users.noreply.github.com>
2025-01-21 12:55:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*
 Goals for the day:
-    1. Review and make objects with nest properties
+    1. Review and make objects with nested properties
     2. Become familiar with Data Types in javascript
     3. Use String Literals to easily format text strings
  */
